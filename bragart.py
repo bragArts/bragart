a31ab52a8aaf949527da00d802c3b38bed7b069f
katielmeyer <klmeyer@live.unc.edu>
2014-04-01 02:25:41 +0000

    got thumbnails working with a static url

--- bragart.py
+++ bragart.py
@@ -87,7 +87,7 @@ class Post(db.Model):
     views = db.Column(db.Integer(), default=0)
     created_at = db.Column(db.DateTime, index=True)
     updated_at = db.Column(db.DateTime)
-    thumbnail = db.Column(db.String(), default="")
+    thumbnail = db.Column(db.String(), default="uploads/lupine_Z5T2WAJC.jpg") #testing url..
 
     def render_content(self):
         _cached = cache.get("post_%s" % self.id)
@@ -113,7 +113,7 @@ class Post(db.Model):
     
     def render_thumbnail(self):
         if self.thumbnail:
-          thumbnailHTML = "<img data-src='{{ url_for('static', filename='img/placeholder-300x200.png') }}' alt='...'>"
+          thumbnailHTML = "<img src='" + self.thumbnail + "' alt='...'>"
           return thumbnailHTML
         else:
           return ""
